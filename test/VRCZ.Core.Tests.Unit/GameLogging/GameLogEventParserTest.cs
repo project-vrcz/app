a7ede77af956a4b039c1c84022625ab1b2d29351
Misaka-L <lipww1234@foxmail.com>
2025-05-25 01:24:54 +0800

    test: add unit tests for VRChat game log event parser

---
+++ test/VRCZ.Core.Tests.Unit/GameLogging/GameLogEventParserTest.cs
@@ -0,0 +1,42 @@
+﻿using VRCZ.Core.GameLogging.LogEvent;
+using VRCZ.Core.Models.VRChat.Logging;
+using VRCZ.Core.Models.VRChat.Logging.LogEvent;
+
+namespace VRCZ.Core.Tests.Unit.GameLogging;
+
+public class GameLogEventParserTest
+{
+    [Fact]
+    public void Parse_ShouldReturnNull_WhenLogEntityMatchNothing()
+    {
+        // Arrange
+
+        var parser = new VRChatGameLogEventParser();
+        var logEntity = new VRChatLogEntity(new DateTime(2023, 10, 1), "Debug", "TestMessage");
+
+        // Act
+        var result = parser.Parse(logEntity);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Parse_ShouldReturnPlayerJoinEvent_WhenLogEntityIsPlayerJoin()
+    {
+        // Arrange
+        var parser = new VRChatGameLogEventParser();
+        var logEntity = new VRChatLogEntity(new DateTime(2023, 10, 1), "Info", "[Behaviour] OnPlayerJoined PlayerNickname (PlayerUserId)");
+
+        // Act
+        var result = parser.Parse(logEntity);
+
+        // Assert
+        Assert.NotNull(result);
+
+        var joinEvent = Assert.IsType<PlayerJoinLogEvent>(result);
+
+        Assert.Equal("PlayerNickname", joinEvent.Nickname);
+        Assert.Equal("PlayerUserId", joinEvent.UserId);
+    }
+}
