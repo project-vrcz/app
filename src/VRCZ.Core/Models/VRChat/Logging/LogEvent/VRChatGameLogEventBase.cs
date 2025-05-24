780d44e97390ec9d55a0fe9d58a670e3791ea54b
Misaka-L <lipww1234@foxmail.com>
2025-05-25 00:36:23 +0800

    test: test for vrchat log event base

--- src/VRCZ.Core/Models/VRChat/Logging/LogEvent/VRChatGameLogEventBase.cs
+++ src/VRCZ.Core/Models/VRChat/Logging/LogEvent/VRChatGameLogEventBase.cs
@@ -1,6 +1,6 @@
 ﻿namespace VRCZ.Core.Models.VRChat.Logging.LogEvent;
 
-public abstract record VRChatGameLogEventBase(VRChatLogEntity LogEntity)
+public record VRChatGameLogEventBase(VRChatLogEntity LogEntity)
 {
     public DateTime Timestamp => LogEntity.Timestamp;
 }
